57fa6d079f4a81b447ed90d4e5c15388deb14cc0
Manssoum Mustapha <mustapha.manssoum@telecom-st-etienne.fr>
2022-10-23 19:35:06 +0200

    Modification of Jarvi's algo code

--- JARVIS_convex_hall.m
+++ JARVIS_convex_hall.m
@@ -3,9 +3,9 @@ clear all; close all; clc;
 %% définition de l'orientation de 2 points:
 orient = @(px,py,qx,qy, rx, ry) sign((qx - px) * (ry - py) - (rx - px) * (qy - py));
 %% Calcul du cos (produit scalaire)
-cosAngle = @(px,py,qx,qy, rx, ry) - ((qx - px) * (rx - qx) + (qy - py) * (ry - qy)) / (sqrt((qx - px)^2 + (qy - py)^2) * sqrt((rx - qx)^2 + (ry - qy)^2));
+cosAngle = @(px,py,qx,qy, rx, ry)  ((qx - px) * (rx - qx) + (qy - py) * (ry - qy)) / (sqrt((qx - px)^2 + (qy - py)^2) * sqrt((rx - qx)^2 + (ry - qy)^2));
 %% Un nuage dde points aléatoire dans le carré [0,1]²
-n = 20;
+n = 30;
 points = rand(2,n);
 
 %% Affichage du nuage
@@ -46,13 +46,13 @@ end
 x = p;
 j = q;
 for i = 1:n
-    min_cos = 1;
+    max_cos = -1;
     for r = 1:n
 
         my_cos = cosAngle(points(1,x),points(2,x),points(1,j),points(2,j),points(1,r),points(2,r));
             
-            if (my_cos < min_cos)
-                min_cos = my_cos;
+            if (my_cos > max_cos)
+                max_cos = my_cos;
                 max_r = r;
             end
        
